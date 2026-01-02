775fc1b471b9701ca22faaa8832471fd40a233ef
Emeraldtheporcupine <125104247+Emeraldtheporcupine@users.noreply.github.com>
2026-01-02 12:47:23 -0600

    Fixed Title bug

--- main.ts
+++ main.ts
@@ -9,23 +9,53 @@ namespace SpriteKind {
     export const Hazards = SpriteKind.create()
     export const Effect = SpriteKind.create()
     export const Point = SpriteKind.create()
+    export const Title = SpriteKind.create()
 }
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    HropeLength = 0
-    HhookSprite = sprites.create(assets.image`hook0`, SpriteKind.Hhook)
-    HhookSprite.setPosition(Robob.x, Robob.y)
-    HhookSprite.vx = 100 * direction
+    if (Screen == "Game") {
+        HropeLength = 0
+        HhookSprite = sprites.create(assets.image`hook0`, SpriteKind.Hhook)
+        HhookSprite.setPosition(Robob.x, Robob.y)
+        HhookSprite.vx = 100 * direction
+    } else {
+    	
+    }
 })
+function Start () {
+    Robob = sprites.create(assets.image`IdleR`, SpriteKind.Player)
+    Robob.ay = 400
+    HhookSprite = sprites.create(assets.image`blank`, SpriteKind.rope)
+    VhookSprite = sprites.create(assets.image`blank1`, SpriteKind.rope)
+    Portal = sprites.create(assets.image`blank`, SpriteKind.Warp)
+    animation.runImageAnimation(
+    Portal,
+    assets.animation`PORTAL`,
+    100,
+    true
+    )
+    direction = 1
+    music.play(music.createSong(assets.song`Level`), music.PlaybackMode.LoopingInBackground)
+    SetupLevel()
+    Screen = "Game"
+}
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Warp, function (sprite, otherSprite) {
     game.splash("Level " + level + " complete")
     level += 1
     SetupLevel()
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    VropeLength = 0
-    VhookSprite = sprites.create(assets.image`hook`, SpriteKind.Vhook)
-    VhookSprite.setPosition(Robob.x, Robob.y)
-    VhookSprite.vy = -100
+    if (Screen == "Game") {
+        VropeLength = 0
+        VhookSprite = sprites.create(assets.image`hook`, SpriteKind.Vhook)
+        VhookSprite.setPosition(Robob.x, Robob.y)
+        VhookSprite.vy = -100
+    } else if (Screen == "Title") {
+        Screen = "Loading..."
+        level = 1
+        sprites.destroyAllSpritesOfKind(SpriteKind.Title)
+        music.stopAllSounds()
+        Start()
+    }
 })
 function SetupLevel () {
     direction = 1
@@ -62,6 +92,7 @@ function SetupLevel () {
         tiles.placeOnTile(Robob, tiles.getTileLocation(1, 3))
         tiles.placeOnTile(Portal, tiles.getTileLocation(1, 1))
     } else if (level == 4) {
+        direction = -1
         tiles.setCurrentTilemap(tileUtil.createSmallMap(tilemap`level3`))
         blue_Portal = sprites.create(assets.image`blank1`, SpriteKind.BluePortal)
         red_Portal = sprites.create(assets.image`blank`, SpriteKind.RedPortal)
@@ -132,21 +163,25 @@ scene.onHitWall(SpriteKind.Vhook, function (sprite, location) {
     sprites.destroy(VhookSprite)
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (level == 1) {
-        if (Robob.tilemapLocation().column == 1 && Robob.tilemapLocation().row == 13) {
-            game.splash("Use 'B' to hook left and right!")
-        } else if (Robob.tilemapLocation().column == 17 && Robob.tilemapLocation().row == 13) {
-            game.splash("Use 'A' to hook up!")
-        } else if (Robob.tilemapLocation().column == 9 && Robob.tilemapLocation().row == 10) {
-            game.splash("Some ceilings can't be hooked onto long.")
-        } else if (Robob.tilemapLocation().column == 2 && Robob.tilemapLocation().row == 10) {
-            game.splash("Touch the portal to advance!")
-        }
-    } else if (level == 2) {
-        if (Robob.tilemapLocation().column == 18 && Robob.tilemapLocation().row == 13) {
-            game.splash("Go on up!")
-        } else if (Robob.tilemapLocation().column == 1 && Robob.tilemapLocation().row == 4) {
-            game.splash("Blue Portals go to Red Portals.")
+    if (Screen == "Game") {
+        if (level == 1) {
+            if (Robob.tilemapLocation().column == 1 && Robob.tilemapLocation().row == 13) {
+                game.splash("Use 'B' to hook left and right!")
+            } else if (Robob.tilemapLocation().column == 17 && Robob.tilemapLocation().row == 13) {
+                game.splash("Use 'A' to hook up!")
+            } else if (Robob.tilemapLocation().column == 9 && Robob.tilemapLocation().row == 10) {
+                game.splash("Some ceilings can't be hooked onto long.")
+            } else if (Robob.tilemapLocation().column == 2 && Robob.tilemapLocation().row == 10) {
+                game.splash("Touch the portal to advance!")
+            }
+        } else if (level == 2) {
+            if (Robob.tilemapLocation().column == 18 && Robob.tilemapLocation().row == 13) {
+                game.splash("Go on up!")
+            } else if (Robob.tilemapLocation().column == 1 && Robob.tilemapLocation().row == 4) {
+                game.splash("Blue Portals go to Red Portals.")
+            }
+        } else {
+        	
         }
     } else {
     	
@@ -165,61 +200,84 @@ let Lava: Sprite = null
 let red_Portal: Sprite = null
 let blue_Portal: Sprite = null
 let VropeLength = 0
-let HropeLength = 0
-let direction = 0
+let level = 0
 let Portal: Sprite = null
 let VhookSprite: Sprite = null
-let HhookSprite: Sprite = null
+let direction = 0
 let Robob: Sprite = null
-let level = 0
+let HhookSprite: Sprite = null
+let HropeLength = 0
+let Screen = ""
+Screen = "Title"
 scene.setBackgroundImage(assets.image`level1`)
-level = 1
-Robob = sprites.create(assets.image`IdleR`, SpriteKind.Player)
-Robob.ay = 400
-HhookSprite = sprites.create(assets.image`blank`, SpriteKind.rope)
-VhookSprite = sprites.create(assets.image`blank1`, SpriteKind.rope)
-Portal = sprites.create(assets.image`blank`, SpriteKind.Warp)
+music.play(music.createSong(assets.song`City Harbor`), music.PlaybackMode.LoopingInBackground)
+let Title = sprites.create(assets.image`Title`, SpriteKind.Title)
+Title.setPosition(80, 25)
+let Press_A = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Title)
+Press_A.setPosition(15, 100)
 animation.runImageAnimation(
-Portal,
-assets.animation`PORTAL`,
+Press_A,
+assets.animation`Press A`,
 100,
 true
 )
-direction = 1
-music.play(music.createSong(assets.song`City Harbor`), music.PlaybackMode.LoopingInBackground)
-SetupLevel()
 game.onUpdate(function () {
-    Robob.z = 2
-    if (controller.right.isPressed()) {
-        direction = 1
-        Robob.setImage(assets.image`IdleR`)
-    }
-    if (controller.left.isPressed()) {
-        direction = -1
-        Robob.setImage(assets.image`IdleL`)
+    if (Screen == "Game") {
+        Robob.z = 2
+        if (controller.right.isPressed()) {
+            direction = 1
+            Robob.setImage(assets.image`IdleR`)
+        }
+        if (controller.left.isPressed()) {
+            direction = -1
+            Robob.setImage(assets.image`IdleL`)
+        }
+    } else {
+    	
     }
 })
 game.onUpdate(function () {
-    if (pointer) {
-        pointer.vy = -25
-        pointer.setFlag(SpriteFlag.AutoDestroy, true)
-    }
-    if (!(controller.A.isPressed())) {
-        VropeLength = 0
-        sprites.destroy(VhookSprite)
-        Robob.ay = 400
-    } else {
-        if (!(Robob.tileKindAt(TileDirection.Top, assets.tile`8`)) && !(Robob.tileKindAt(TileDirection.Top, assets.tile`transparency8`))) {
-            Robob.ay = 0
-        } else {
-            Robob.ay = 400
+    if (Screen == "Game") {
+        if (pointer) {
+            pointer.vy = -25
+            pointer.setFlag(SpriteFlag.AutoDestroy, true)
         }
-    }
-    if (!(controller.B.isPressed())) {
-        HropeLength = 0
-        sprites.destroy(HhookSprite)
-    }
-    if (Robob.vy > 0) {
-        sprites.destroy(HhookSprite)
+        if (!(controller.A.isPressed())) {
+            VropeLength = 0
+            sprites.destroy(VhookSprite)
+            Robob.ay = 400
+        } else {
+            if (!(Robob.tileKindAt(TileDirection.Top, assets.tile`8`)) && !(Robob.tileKindAt(TileDirection.Top, assets.tile`transparency8`))) {
+                Robob.ay = 0
+            } else {
+                Robob.ay = 400
+            }
+        }
+        if (!(controller.B.isPressed())) {
+            HropeLength = 0
+            sprites.destroy(HhookSprite)
+        }
+        if (Robob.vy > 0) {
+            sprites.destroy(HhookSprite)
+        }
+    } else {
+    	
     }
 })
